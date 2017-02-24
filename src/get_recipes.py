2f08606139eeeca856c98486e04de5f214af3a46
Ryan Lee <ryantlee9@gmail.com>
2017-02-24 01:11:52 -0500

    Add FoodNetwork scraper

--- src/get_recipes.py
+++ src/get_recipes.py
@@ -5,17 +5,25 @@ from urllib.error import HTTPError
 from bs4 import BeautifulSoup
 
 import sys
+from os import path
+
 import config
 sys.path.append(config.path_scrapers)
 from recipe_scrapers import scrap_me
 
-base_url = 'http://www.epicurious.com'
+HEADERS = {
+    'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
+}
 
 def get_recipe(url):
-    scrap = scrap_me(url)
+    try:
+        scrap = scrap_me(url)
+    except:
+        print('Could not scrape URL {}'.format(url))
+        return {}
 
     try:
-        title = scrap.title()[0],
+        title = scrap.title()
     except AttributeError:
         title = None
 
@@ -30,7 +38,7 @@ def get_recipe(url):
         instructions = None
 
     try:
-        picture_link = scrap.picture()[0]
+        picture_link = scrap.picture()
     except AttributeError:
         picture_link = None
 
@@ -41,11 +49,24 @@ def get_recipe(url):
         'picture_link': picture_link,
     }
 
-def get_all_recipes(page_num):
-    # some sites close their content for 'bots', so user-agent must be supplied
-    HEADERS = {
-        'User-Agent': 'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.9.0.7) Gecko/2009021910 Firefox/3.0.7'
-    }
+def get_all_recipes_fn(page_str, page_num):
+    base_url = 'http://www.foodnetwork.com'
+    search_url_str = 'recipes/a-z'
+    url = '{}/{}/{}/p/{}'.format(base_url, search_url_str, page_str, page_num)
+
+    try:
+        soup = BeautifulSoup(request.urlopen(
+            request.Request(url, headers=HEADERS)).read(), "html.parser")
+        recipe_link_items = soup.select('div.o-Capsule__m-Body ul.m-PromoList li a')
+        recipe_links = [r.attrs['href']for r in recipe_link_items]
+        print('Read {} recipe links from {}'.format(len(recipe_links), url))
+        return {r: get_recipe(r) for r in recipe_links}
+    except HTTPError:
+        print('Could not parse page {}'.format(url))
+        return []
+
+def get_all_recipes_epi(page_num):
+    base_url = 'http://www.epicurious.com'
     search_url_str = 'search/?content=recipe&page'
     url = '{}/{}={}'.format(base_url, search_url_str, page_num)
 
@@ -60,20 +81,52 @@ def get_all_recipes(page_num):
         return []
 
 
-def main(start_page=1, num_pages=1916, status_interval=50):
-    recipes_epi = {}
+def scrape_epi(start_page=1, num_pages=1916, status_interval=50):
 
+    recipes_epi = {}
     start = time.time()
     for i in range(start_page, num_pages + start_page):
-        recipes_epi.update(get_all_recipes(i))
+        recipes_epi.update(get_all_recipes_epi(i))
         if i % status_interval == 0:
             print('Scraping page {} of {}'.format(i + 1 - start_page, num_pages))
     print('Scraped {} recipes from {} in {:.0f} minutes'.format(
-        len(recipes_epi), base_url, (time.time() - start) / 60))
+        len(recipes_epi), 'Epicurious.com', (time.time() - start) / 60))
 
     # Save to disk as JSON
-    with open('recipes_raw.json', 'w') as f:
+    with open(path.join(config.path_data, 'recipes_raw_epi.json'), 'w') as f:
         json.dump(recipes_epi, f)
 
+
+def scrape_fn():
+    recipes_fn = {}
+    start = time.time()
+
+    # get list of pages with links to recipes
+    base_url = 'http://www.foodnetwork.com'
+    search_url_str = 'recipes/a-z'
+    url = '{}/{}/{}'.format(base_url, search_url_str, '')
+
+    try:
+        soup = BeautifulSoup(request.urlopen(
+            request.Request(url, headers=HEADERS)).read(), "html.parser")
+        page_link_items = soup.select('ul.o-IndexPagination__m-List li a')
+        page_links = [p['href'] for p in page_link_items]
+    except HTTPError:
+        print('Could not parse page {}'.format(url))
+
+    for i, page in enumerate(page_links):
+        page_num = 1
+        recipe_set = True
+        while recipe_set:
+            recipe_set = get_all_recipes_fn(path.basename(page), page_num)
+            recipes_fn.update(recipe_set)
+            page_num += 1
+    print('Scraped {} recipes from {} in {:.0f} minutes'.format(
+        len(recipes_fn), 'Epicurious.com', (time.time() - start) / 60))
+
+    # Save to disk as JSON
+    with open(path.join(config.path_data, 'recipes_raw_fn.json'), 'w') as f:
+        json.dump(recipes_fn, f)
+
 if __name__ == '__main__':
-    main(status_interval=1)
+    scrape_fn()
